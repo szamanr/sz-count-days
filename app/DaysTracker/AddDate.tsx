f0e78f0c8f8805dd0816d6cb94cffb372f3d482e
szamanr <szamanr@protonmail.com>
2024-04-04 13:11:51 +0200

    add validation with zod

--- app/DaysTracker/AddDate.tsx
+++ app/DaysTracker/AddDate.tsx
@@ -1,16 +1,23 @@
-import { Component } from "solid-js";
+import { Component, Show } from "solid-js";
 import { Popover } from "@ark-ui/solid";
 import { Input } from "common/Input.tsx";
 import { Button } from "common/Button.tsx";
-import { createForm, SubmitHandler } from "@modular-forms/solid";
+import { createForm, SubmitHandler, zodForm } from "@modular-forms/solid";
+import { z } from "zod";
 
-type DateForm = {
-  name?: string;
-  date: string;
-};
+const schema = z.object({
+  date: z
+    .string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/, "date must follow the format: 2012-08-06"),
+  name: z.string().optional(),
+});
+
+type DateForm = z.infer<typeof schema>;
 
 export const AddDate: Component = () => {
-  const [dateForm, { Form, Field, FieldArray }] = createForm<DateForm>();
+  const [_, { Form, Field }] = createForm<DateForm>({
+    validate: zodForm(schema),
+  });
 
   const addDate: SubmitHandler<DateForm> = (form) => {
     console.debug(form.name, form.date);
@@ -20,34 +27,35 @@ export const AddDate: Component = () => {
     <Popover.Root>
       <Popover.Trigger class="w-full">Pick new date</Popover.Trigger>
       <Popover.Positioner>
-        <Popover.Content class="min-w-64 rounded bg-stone-600 p-4">
+        <Popover.Content class="min-w-96 rounded bg-stone-600 p-4">
           <Popover.Title>Pick a date</Popover.Title>
           <Form onSubmit={addDate}>
             <Popover.Description class="space-y-2 py-2">
-              <div class="grid grid-cols-3">
-                <Field name="name">
-                  {(_, props) => (
+              <Field name="name">
+                {(field, props) => (
+                  <div class="flex w-full flex-col">
+                    <Input type="text" label="Name" {...props} />
+                    <Show when={field.error}>
+                      <span class="text-red-500">{field.error}</span>
+                    </Show>
+                  </div>
+                )}
+              </Field>
+              <Field name="date">
+                {(field, props) => (
+                  <div class="flex w-full flex-col">
                     <Input
-                      outerClass="col-span-2"
-                      type="text"
-                      label="Name"
-                      {...props}
-                    />
-                  )}
-                </Field>
-              </div>
-              <div class="grid grid-cols-3">
-                <Field name="date">
-                  {(_, props) => (
-                    <Input
-                      outerClass="col-span-2"
                       type="date"
                       label="Date"
+                      required={true}
                       {...props}
                     />
-                  )}
-                </Field>
-              </div>
+                    <Show when={field.error}>
+                      <span class="text-red-500">{field.error}</span>
+                    </Show>
+                  </div>
+                )}
+              </Field>
               <Button type="submit">Add</Button>
             </Popover.Description>
           </Form>
