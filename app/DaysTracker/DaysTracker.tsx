a50c8d3b7984e82c97c73fc41c66a79cee00c4a2
szamanr <szamanr@protonmail.com>
2024-04-04 13:42:50 +0200

    implement removing

--- app/DaysTracker/DaysTracker.tsx
+++ app/DaysTracker/DaysTracker.tsx
@@ -2,14 +2,23 @@ import { createSignal, For } from "solid-js";
 import { SavedDate } from "./types";
 import { Day } from "./Day";
 import { AddDate } from "./AddDate";
+import { Button } from "common/Button.tsx";
+import { without } from "lodash";
 
 export const DaysTracker = () => {
   const localStorageDates: SavedDate[] = JSON.parse(
     window.localStorage.getItem("savedDates") ?? "[]",
   );
   const [dates, setDates] = createSignal(localStorageDates);
+
   const addDate = (date: SavedDate) => {
     const newDates = [...dates(), date];
+    setDates(newDates);
+    window.localStorage.setItem("savedDates", JSON.stringify(newDates));
+  };
+
+  const removeDate = (date: SavedDate) => {
+    const newDates = without([...dates()], date);
     setDates(newDates);
     window.localStorage.setItem("savedDates", JSON.stringify(newDates));
   };
@@ -22,7 +31,16 @@ export const DaysTracker = () => {
         <For each={dates()} fallback={fallback}>
           {(date) => (
             <li>
-              <Day date={date.date} name={date.name} />
+              <div class="group flex items-center space-x-1">
+                <Button
+                  class="invisible text-xl leading-none text-red-500 hover:text-red-800 group-hover:visible"
+                  onClick={removeDate.bind(null, date)}
+                  variant="negative"
+                >
+                  ✖
+                </Button>
+                <Day date={date.date} name={date.name} />
+              </div>
             </li>
           )}
         </For>
