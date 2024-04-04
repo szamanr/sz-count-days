93ddccee4cb89601e97e02ae12be5effff767cee
szamanr <szamanr@protonmail.com>
2024-04-04 13:53:18 +0200

    use icon for delete

--- app/DaysTracker/DaysTracker.tsx
+++ app/DaysTracker/DaysTracker.tsx
@@ -2,8 +2,9 @@ import { createSignal, For } from "solid-js";
 import { SavedDate } from "./types";
 import { Day } from "./Day";
 import { AddDate } from "./AddDate";
-import { Button } from "common/Button.tsx";
+import { Button } from "common/Button";
 import { without } from "lodash";
+import { Icon } from "common/Icon";
 
 export const DaysTracker = () => {
   const localStorageDates: SavedDate[] = JSON.parse(
@@ -23,7 +24,7 @@ export const DaysTracker = () => {
     window.localStorage.setItem("savedDates", JSON.stringify(newDates));
   };
 
-  const fallback = <p>Add a date below</p>;
+  const fallback = <p class="text-gray-500">Add a date below</p>;
 
   return (
     <main class="flex w-full flex-col items-center space-y-4 p-8">
@@ -33,11 +34,11 @@ export const DaysTracker = () => {
             <li>
               <div class="group flex items-center space-x-1">
                 <Button
-                  class="invisible text-xl leading-none text-red-500 hover:text-red-800 group-hover:visible"
+                  class="invisible text-red-500 hover:text-red-800 group-hover:visible"
                   onClick={removeDate.bind(null, date)}
                   variant="negative"
                 >
-                  ✖
+                  <Icon name="close" size="xl" />
                 </Button>
                 <Day date={date.date} name={date.name} />
               </div>
