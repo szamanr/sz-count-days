9af6d347687c9d529e904b41aab474fd1ad71477
szaman <2622838+szamanr@users.noreply.github.com>
2024-04-21 17:15:09 +0000

    show overlapping trips in summary (#22)
    show overlapping trips in summary
    
    extract OverlapTooltip component

---
+++ app/Schengen/OverlapTooltip.tsx
@@ -0,0 +1,23 @@
+import { Tooltip } from "@ark-ui/solid";
+import { Icon } from "common/Icon";
+import { Portal } from "solid-js/web";
+import { For } from "solid-js";
+
+export const OverlapTooltip = (props: {
+  trips: { name?: string; dates: string }[];
+}) => (
+  <Tooltip.Root closeDelay={100} openDelay={100}>
+    <Tooltip.Trigger class="flex">
+      <Icon name="warning" class="text-red-500" />
+    </Tooltip.Trigger>
+    <Portal>
+      <Tooltip.Positioner>
+        <Tooltip.Content class="z-10 rounded bg-gray-600 p-4 text-white">
+          <For each={props.trips}>
+            {(trip) => <p>overlaps with {trip.name || trip.dates}</p>}
+          </For>
+        </Tooltip.Content>
+      </Tooltip.Positioner>
+    </Portal>
+  </Tooltip.Root>
+);
