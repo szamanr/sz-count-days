e9da2b335465cba63c11d6ac837d87591307900f
szamanr <szamanr@protonmail.com>
2024-04-21 18:26:11 +0200

    replace duration setting with radio buttons

--- app/DaysTracker/Menu/MenuSettings.tsx
+++ app/DaysTracker/Menu/MenuSettings.tsx
@@ -1,18 +1,16 @@
-import { Button } from "common/Button";
-import { Icon } from "common/Icon";
 import { Component } from "solid-js";
 import { Settings } from "app/DaysTracker/types";
 
 type Props = {
   settings: Settings;
-  toggleDisplayDurationInDays: () => void;
+  setDisplayDurationInDays: (value: boolean) => void;
   toggleIncludeLastDay: () => void;
 };
 
 export const MenuSettings: Component<Props> = (props) => (
-  <div class="flex w-full flex-col items-center space-y-2 border-b pb-2">
+  <div class="flex w-full flex-col items-start space-y-2 border-b border-stone-300 px-2 pb-4">
     <div>
-      <label class="space-x-1">
+      <label class="space-x-2">
         <input
           type="checkbox"
           onClick={props.toggleIncludeLastDay}
@@ -21,23 +19,23 @@ export const MenuSettings: Component<Props> = (props) => (
         <span>Include last day</span>
       </label>
     </div>
-    <div>
-      <p>
-        <span>Displaying as </span>
-        <span class="italic">
-          {props.settings.displayDurationInDays
-            ? "81 days"
-            : "2 months 20 days"}
-        </span>
-      </p>
-      <Button onClick={props.toggleDisplayDurationInDays} variant="negative">
-        <Icon
-          class="text-teal-400 hover:text-teal-800"
-          name="swap_horiz"
-          size="xl"
-        />
-        <span>Toggle</span>
-      </Button>
+    <div class="space-x-2">
+      <input
+        id="asDays"
+        type="radio"
+        checked={props.settings.displayDurationInDays}
+        onClick={props.setDisplayDurationInDays.bind(null, true)}
+      />
+      <label for="asDays">Display as 81 days</label>
+    </div>
+    <div class="space-x-2">
+      <input
+        id="asDuration"
+        type="radio"
+        checked={!props.settings.displayDurationInDays}
+        onClick={props.setDisplayDurationInDays.bind(null, false)}
+      />
+      <label for="asDuration">Display as 2 months 20 days</label>
     </div>
   </div>
 );
