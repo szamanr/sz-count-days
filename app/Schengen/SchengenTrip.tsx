4af057850daf5957677f5357d153b4d622be74e5
szamanr <szamanr@protonmail.com>
2024-04-23 10:30:51 +0200

    mark overlapping trips on the list

--- app/Schengen/SchengenTrip.tsx
+++ app/Schengen/SchengenTrip.tsx
@@ -1,5 +1,6 @@
 import {
   addDays,
+  areIntervalsOverlapping,
   differenceInCalendarDays,
   format,
   isAfter,
@@ -7,50 +8,71 @@ import {
   isSameDay,
 } from "date-fns";
 import { Strong } from "common/Strong";
-import { JSX, Show } from "solid-js";
+import { Accessor, For, JSX, Show } from "solid-js";
 import { twClass } from "common/twClass";
 import { SchengenDate } from "app/Schengen/types";
 import { formattedDate } from "common/formattedDate";
+import { Icon } from "common/Icon";
+import { isEqual } from "lodash";
 
 const diff = (start: Date | string, end: Date | string) =>
   `${differenceInCalendarDays(end, start)} days`;
 
-type Props = SchengenDate & {
+type Props = {
   class?: string;
   daysRemainingAt: (date: Date | string) => number;
+  trip: SchengenDate;
+  otherTrips: Accessor<SchengenDate[]>;
 };
 
 export const SchengenTrip = (props: Props) => {
   const now = format(new Date(), "yyyy-MM-dd");
-  const endDate = isAfter(props.endDate, props.date) ? props.endDate : now;
+  const endDate = isAfter(props.trip.endDate, props.trip.date)
+    ? props.trip.endDate
+    : now;
   const endDateForDiff = () => addDays(endDate, 1);
 
-  const duration = () => differenceInCalendarDays(endDateForDiff(), props.date);
+  const duration = () =>
+    differenceInCalendarDays(endDateForDiff(), props.trip.date);
   const formattedDuration = () => `${duration()} days`;
 
-  const isOverstay = () => duration() > props.daysRemainingAt(props.date);
+  const isOverstay = () => duration() > props.daysRemainingAt(props.trip.date);
   const className = () =>
     isOverstay() ? twClass(props.class, "text-red-500") : props.class;
   const error = () =>
     isOverstay() ? (
       <span class="block text-sm">
-        Cannot stay longer than {props.daysRemainingAt(props.date)} days.
+        Cannot stay longer than {props.daysRemainingAt(props.trip.date)} days.
       </span>
     ) : undefined;
 
+  const overlappingTrips = () =>
+    props
+      .otherTrips()
+      .filter((trip) => !isEqual(trip, props.trip))
+      .filter((trip) =>
+        areIntervalsOverlapping(
+          { start: props.trip.date, end: endDate },
+          {
+            start: trip.date,
+            end: trip.endDate,
+          },
+        ),
+      );
+
   let relativeDuration: JSX.Element;
 
-  const isFuture = isAfter(props.date, now);
+  const isFuture = isAfter(props.trip.date, now);
   const isPast = isBefore(endDate, now);
 
   if (isFuture) {
     relativeDuration = (
       <>
         <span>It's </span>
-        <span>{diff(now, props.date)}</span>
+        <span>{diff(now, props.trip.date)}</span>
         <span> until </span>
-        <Show when={props.name} fallback={<span>trip</span>}>
-          <Strong>{props.name}</Strong>
+        <Show when={props.trip.name} fallback={<span>trip</span>}>
+          <Strong>{props.trip.name}</Strong>
         </Show>
       </>
     );
@@ -60,8 +82,8 @@ export const SchengenTrip = (props: Props) => {
         <span>It's been </span>
         <span>{diff(endDateForDiff(), now)}</span>
         <span> since </span>
-        <Show when={props.name} fallback={<span>trip</span>}>
-          <Strong>{props.name}</Strong>
+        <Show when={props.trip.name} fallback={<span>trip</span>}>
+          <Strong>{props.trip.name}</Strong>
         </Show>
       </>
     );
@@ -70,15 +92,15 @@ export const SchengenTrip = (props: Props) => {
   else {
     relativeDuration = (
       <>
-        <Show when={!isSameDay(props.date, now)}>
+        <Show when={!isSameDay(props.trip.date, now)}>
           <span>It's been </span>
-          <span>{diff(props.date, now)}</span>
+          <span>{diff(props.trip.date, now)}</span>
           <span> since </span>
         </Show>
-        <Show when={props.name} fallback={<span>trip</span>}>
-          <Strong>{props.name}</Strong>
+        <Show when={props.trip.name} fallback={<span>trip</span>}>
+          <Strong>{props.trip.name}</Strong>
         </Show>
-        <Show when={isSameDay(props.date, now)}>
+        <Show when={isSameDay(props.trip.date, now)}>
           <span> starts today</span>
         </Show>
         <span>, </span>
@@ -93,10 +115,26 @@ export const SchengenTrip = (props: Props) => {
       {relativeDuration}
       <span>
         {" "}
-        ({formattedDate(props.date)} - {formattedDate(endDate)},{" "}
+        ({formattedDate(props.trip.date)} - {formattedDate(endDate)},{" "}
         {formattedDuration()})
       </span>
       {error()}
+      <For each={overlappingTrips()}>
+        {(trip) => {
+          const tripDates = `${formattedDate(trip.date)} - ${formattedDate(trip.endDate)}`;
+          return (
+            <p class="flex items-center space-x-1 text-red-500">
+              <Icon name="warning" />
+              <span>overlaps with </span>
+              <span>
+                <Show when={trip.name} fallback={tripDates}>
+                  {trip.name} ({tripDates})
+                </Show>
+              </span>
+            </p>
+          );
+        }}
+      </For>
     </p>
   );
 };
