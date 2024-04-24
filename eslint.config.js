6a0a812766118c5847b88f1cc816023276930f0f
szamanr <szamanr@protonmail.com>
2024-04-24 13:49:56 +0200

    configure import groups for sort & add global ignore

--- eslint.config.js
+++ eslint.config.js
@@ -4,14 +4,37 @@ import simpleImportSort from "eslint-plugin-simple-import-sort";
 import solid from "eslint-plugin-solid/configs/typescript.js";
 import tseslint from "typescript-eslint";
 
-/** @type { import("eslint").Linter.FlatConfig[] } */
+/**
+ * this is for simple-import-sort plugin. imports will be grouped before sorting.
+ * it's the default plugin config, plus one custom group (project imports)
+ * @type {string[][]}
+ */
+const importGroups = [
+  [
+    // side effect imports
+    "^\\u0000",
+    // packages
+    "^@?\\w",
+    // project imports
+    "^(@|app|common)(/.*|$)",
+    // anything else
+    "^",
+    // relative imports
+    "^\\.",
+  ],
+];
+
+/** @type { import('eslint').Linter.FlatConfig[] } */
 export default tseslint.config(
+  {
+    ignores: ["dist/*", "node_modules/*"],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
   {
-    files: ["**/*.{ts,tsx}"],
     ...solid,
+    files: ["{app,common}/*.{ts,tsx}"],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
@@ -21,9 +44,14 @@ export default tseslint.config(
     plugins: {
       "simple-import-sort": simpleImportSort,
     },
+    ...eslint.configs.recommended,
     rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: importGroups,
+        },
+      ],
     },
   },
 );
