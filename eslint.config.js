a7cbb7185acd6c5bff3f17530fe53b73167137c3
szamanr <szamanr@protonmail.com>
2024-04-24 13:16:40 +0200

    add import sort plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import solid from "eslint-plugin-solid/configs/typescript.js";
 import prettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import solid from "eslint-plugin-solid/configs/typescript.js";
+import tseslint from "typescript-eslint";
 
 /** @type { import("eslint").Linter.FlatConfig[] } */
 export default tseslint.config(
@@ -16,6 +17,13 @@ export default tseslint.config(
       parserOptions: {
         project: "tsconfig.json",
       },
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
 );
