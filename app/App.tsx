0ab0756a3b45ec3a0fbf7daf0661d2fb79bba6be
szamanr <szamanr@protonmail.com>
2024-07-11 12:25:02 +0200

    add vercel analytics

--- app/App.tsx
+++ app/App.tsx
@@ -1,8 +1,17 @@
 import { Route, Router } from "@solidjs/router";
+import { inject } from "@vercel/analytics";
+import { onMount } from "solid-js";
 import { DaysTracker } from "./DaysTracker/DaysTracker";
 import { Schengen } from "./Schengen/Schengen";
 
 const App = () => {
+  onMount(() => {
+    const isDev = import.meta.env.VITE_DEV
+      ? JSON.parse(import.meta.env.VITE_DEV)
+      : false;
+    inject({ mode: isDev ? "development" : "production" });
+  });
+
   return (
     <div class="min-h-screen w-full bg-gray-800 text-white">
       <Router>
